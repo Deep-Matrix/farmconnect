0dd525f831f7d0cbd1bbb068e730c07da020bcf0
amandesai01 <amandesai01@gmail.com>
2020-01-18 22:42:16 +0530

    updated changes. Login Done Interface2 Done. changes in Stream Produce

--- interface2/interface/apis/farmer.py
+++ interface2/interface/apis/farmer.py
@@ -3,7 +3,7 @@ import uuid
 
 def getNameFromId(conn, id):
     #(No Need to connect again and again. just pass values) conn = sqlite3.connect("././datahouse.db")
-    cursor = conn.execute("SELECT FULLNAME FROM farmer WHERE FARMERID = " + str(id))
+    cursor = conn.execute("SELECT FULLNAME FROM farmer WHERE FARMERID == ?" , (id, ))
     row = cursor.fetchone()
     return row[0]
 
